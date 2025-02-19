748d8dc716f84105cc550aae1a050ec6473b6e59
2-adic <two.adic@gmail.com>
2025-02-19 01:13:57 -0500

    Update: New functions to launch a process, and to check if a file is in a directory.

--- src/utils.h
+++ src/utils.h
@@ -2,11 +2,16 @@
 #define UTILS_H
 
 #include "config.h"
+#include <stdbool.h>
 
 void printInput(struct userInput* input);
 
 void strcatReplace(char str1[PATH_MAX + 1], char replace[PATH_MAX + 1]);
 
 int compareEntries(const void *a, const void *b);
+
+bool isFileInDirectory(const char *filename, const char *dir);
+
+void runProcess(char dir[PATH_MAX + 1], char **commands, bool is_redirect, char redirect[MAX_CMD_LENGTH + 1]);
 
 #endif
