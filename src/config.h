af3096e873bcfdbe4bab0525961c52ba0a9a969a
2-adic <two.adic@gmail.com>
2025-02-19 00:58:18 -0500

    Update: Better error handling using jumps.

--- src/config.h
+++ src/config.h
@@ -1,16 +1,21 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <stdbool.h>
+#include <setjmp.h>
+
+jmp_buf errorJump;
+
 #define PATH_MAX 4096
 #define MAX_CMD_AMOUNT 128
 #define MAX_CMD_LENGTH 255
 
 struct userInput {
-    char commands[MAX_CMD_AMOUNT][MAX_CMD_LENGTH + 1];
-    char std_redirect[MAX_CMD_AMOUNT][MAX_CMD_LENGTH + 1];
+    char **commands;
+    char redirect[MAX_CMD_LENGTH + 1];
 
     int commands_size;
-    int std_redirect_size;
+    bool is_redirect;
 };
 
 #endif
