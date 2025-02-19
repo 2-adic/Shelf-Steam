1c1998604e69b85bc33118f223a23ce79b0046b3
2-adic <two.adic@gmail.com>
2025-02-19 01:11:16 -0500

    Update: New function to launch games.

--- src/shell_cmds.h
+++ src/shell_cmds.h
@@ -11,6 +11,9 @@ void getUserInput(struct userInput* input);
 // lists all files in lexicographical order
 void lsCommand(const char *dir_path);
 
+// used to launch a game or replay a past game
+void launchGame(char dir[PATH_MAX + 1], char **commands, bool is_redirect, char redirect[MAX_CMD_LENGTH + 1]);
+
 // displays error from incorrect input
 void handleError(bool is_exit);
 
