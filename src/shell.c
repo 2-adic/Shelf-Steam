43b93c0c861a10a9fe94d9956941d50fab21cdd7
2-adic <two.adic@gmail.com>
2025-02-20 19:28:14 -0500

    Update: Small functionality changes.

--- src/shell.c
+++ src/shell.c
@@ -10,14 +10,19 @@
 #include <unistd.h>
 #include <dirent.h>
 
-void shellInitialize(int argc, char *argv[], char dir[PATH_MAX + 1]) {
+void shellInitialize(int argc, char *argv[], char *dir) {
     // the path of the games directory must be included
     if (argc != 2) {
         handleError(1);
     }
 
     // gets the game directory
-    strncpy(dir, argv[1], PATH_MAX);
+    strcpy(dir, argv[1]);
+
+    // exits program if directory is invalid
+    if (!isDirectoryValid(dir)) {
+        handleError(1);
+    }
 }
 
 void shellLoop(char dir[PATH_MAX + 1]) {
@@ -28,7 +33,7 @@ void shellLoop(char dir[PATH_MAX + 1]) {
         struct userInput* input = malloc(sizeof(struct userInput));
         getUserInput(input); // gets user input
 
-        // printInput(input); // DELETE LATER
+        fflush(stdout); // flush to stdout after asking user for input
         
         // pointers here are used as simpler access to input
         char **cmds = input->commands;
@@ -37,37 +42,35 @@ void shellLoop(char dir[PATH_MAX + 1]) {
 
         // exits the program is user types "exit"
         if (!strcmp(cmds[0], "exit")) {
+            if (cmds_size != 1) {
+                handleError(0); // if user types the exit command incorrectly, the program is not exited
+            }
             exit(0);
         }
 
         else if (!strcmp(cmds[0], "path")) {
             // path command must have 1 argument
-            if (cmds_size != 2 || is_rdt) {
-                handleError(1);
+            if (cmds_size != 2 || is_rdt || !isDirectoryValid(cmds[1])) {
+                handleError(0);
             }
             
             strncpy(dir, cmds[1], PATH_MAX); // replaces the current dir
         }
 
+        // prints all files found within the dir with info about each one
         else if (!strcmp(cmds[0], "ls")) {
             lsCommand(dir);
         }
 
-        else if (!strcmp(cmds[0], "test")) {
-            printf("this is a test\n");
-        }
-
         else {
             // launches games
-            char *dir_temp = "/bin/"; 
-            strcatReplace(&dir, &dir_temp);
-            if (isFileInDirectory(cmds[0], dir_temp)) { // checks if the first argument is a valid game name
+            if (isFileInDirectory(cmds[0], dir)) { // checks if the first argument is a valid game name
                 launchGame(dir, cmds, is_rdt, input->redirect);
             } 
 
-            // unknown arguments are invalid and exit the program
+            // unknown arguments are invalid
             else {
-                handleError(1);
+                handleError(0);
             }
         }
 
