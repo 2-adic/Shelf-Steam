830be49af30b0a1d33f0833c1202c868d2eb2026
2-adic <two.adic@gmail.com>
2025-02-20 19:25:49 -0500

    Update: Removed strcatReplace(), runProcess() parameter type change, new functions.

--- src/utils.h
+++ src/utils.h
@@ -4,14 +4,22 @@
 #include "config.h"
 #include <stdbool.h>
 
+// prints user input for testing
 void printInput(struct userInput* input);
 
-void strcatReplace(char **str1, char **replace);
-
+// used for lexicographic sorting with qsort
 int compareEntries(const void *a, const void *b);
 
+// returns if a file is within a directory
 bool isFileInDirectory(const char *filename, const char *dir);
 
-void runProcess(char dir[PATH_MAX + 1], char **commands, bool is_redirect, char redirect[MAX_CMD_LENGTH + 1]);
+// runs a given program as a process (forks the program to run the process)
+void runProcess(char *dir, char **commands, bool is_redirect, const char *redirect, bool is_ls);
+
+// returns if a string contains only white spaces (' ', '\n', '\t')
+bool isAllWhitespace(const char *str);
+
+// returns if a directory is found at the given path
+bool isDirectoryValid(const char *dir);
 
 #endif
