8676e953ee81761b6a64f0f5a2a57ec7cc5c8ac7
2-adic <two.adic@gmail.com>
2025-02-20 19:19:17 -0500

    Update: Moved errorJump to config.c.

--- src/config.h
+++ src/config.h
@@ -4,12 +4,13 @@
 #include <stdbool.h>
 #include <setjmp.h>
 
-jmp_buf errorJump;
+extern jmp_buf errorJump; // used for error handling to jump to the start of the while loop
 
 #define PATH_MAX 4096
 #define MAX_CMD_AMOUNT 128
 #define MAX_CMD_LENGTH 255
 
+// used to store user input
 struct userInput {
     char **commands;
     char redirect[MAX_CMD_LENGTH + 1];
